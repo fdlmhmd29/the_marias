368fec88368029799ab4231054d62c071e15852c
JilanFerdiansyah <72718434+JilanFerdiansyah@users.noreply.github.com>
2021-09-23 20:08:00 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    smartphone: {
+        smooth: true
+    }
+});
+
